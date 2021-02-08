9e1edf7a06bb281a77a80ee97ae63dd251d9d519
Sultan Mahmud <smrsaimon960@gmail.com>
2021-02-08 21:14:15 +0600

    songsupdate

---
+++ app.js
@@ -0,0 +1,44 @@
+const $ = (ID) => document.getElementById(ID);
+
+const searchSongs = () => {
+    const inputSonageName = $('songs').value;
+
+    fetch(`https://api.lyrics.ovh/suggest/${inputSonageName}`)
+        .then((response) => response.json())
+
+    .then((data) => {
+        const songList = data.data;
+
+        const container = $('container');
+
+        let HTMLtemplate = '';
+        songList.forEach((x) => {
+            HTMLtemplate += `
+           <div class="single-result row align-items-center my-3 p-3">
+                <div class="col-md-9">
+                    <h3 class="lyrics-name">${x.title}</h3>
+                    <p class="author lead">Album by <span>${x.artist.name}</span></p>
+                    <audio controls>
+								<source src="${x.preview}" type="audio/ogg" />
+							</audio>
+                </div>
+                <div class="col-md-3 text-md-right text-center">
+                    <button  onclick="getLyrics('${x.artist.name}','${x.title}')" class="btn btn-success">Get Lyrics</button>
+                </div>
+            </div>
+             
+          
+          `;
+        });
+
+        container.innerHTML = HTMLtemplate;
+    });
+};
+
+const getLyrics = (name, title) => {
+    fetch(`https://api.lyrics.ovh/v1/${name}/${title}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        });
+};
